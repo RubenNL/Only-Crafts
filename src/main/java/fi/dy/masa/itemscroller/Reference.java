c4fea93314c93115d12da5b7d95b5b34537bf2d0
mexiscool <mexiscool@gmail.com>
2021-04-17 05:22:34 +0200

    stuff

--- src/main/java/fi/dy/masa/itemscroller/Reference.java
+++ src/main/java/fi/dy/masa/itemscroller/Reference.java
@@ -2,7 +2,7 @@ package fi.dy.masa.itemscroller;
 
 public class Reference
 {
-    public static final String MOD_ID = "itemscroller";
-    public static final String MOD_NAME = "Item Scroller";
+    public static final String MOD_ID = "only-crafts";
+    public static final String MOD_NAME = "Only Crafts";
     public static final String MOD_VERSION = "@MOD_VERSION@";
 }
